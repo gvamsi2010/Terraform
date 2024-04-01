1f1f584e1d1117392828616cfe0aadbd93ade8c7
gvamsi2010 <gvamsi2010@gmail.com>
2024-04-01 14:36:43 -0400

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -2,3 +2,13 @@
 provider "aws" {
   region = "us-east-1"  # Change this to your desired AWS region
 }
+
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 1.0.4"
+    }
+  }
+}
